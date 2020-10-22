51bc8931ee3f8116ad21e041388d18bd5355df44
Janelle <dominja@oocl.com>
2020-10-22 22:49:10 +0800

    [JPA][Cedric-Janelle] test: fixed unit test for company repository

--- src/test/java/com/thoughtworks/springbootemployee/CompanyServiceTest.java
+++ src/test/java/com/thoughtworks/springbootemployee/CompanyServiceTest.java
@@ -2,11 +2,16 @@ package com.thoughtworks.springbootemployee;
 
 import com.thoughtworks.springbootemployee.models.Company;
 import com.thoughtworks.springbootemployee.models.Employee;
-import com.thoughtworks.springbootemployee.repositories.CompanyRepositoryLegacy;
+import com.thoughtworks.springbootemployee.repository.ICompanyRepository;
 import com.thoughtworks.springbootemployee.services.CompanyService;
 import org.junit.jupiter.api.Test;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 import static java.util.Arrays.asList;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -22,8 +27,21 @@ class CompanyServiceTest {
     @Test
     void should_return_2_companies_when_get_companies_given_2_companies() {
         //given
-        CompanyRepositoryLegacy companyRepository = mock(CompanyRepositoryLegacy.class);
-        when(companyRepository.getAll()).thenReturn(asList(new Company(1, "OOCL"), new Company(2, "SM")));
+        ICompanyRepository companyRepository = mock(ICompanyRepository.class);
+
+        List<Employee> employeeList = Arrays.asList(
+                new Employee("nelly", 18, "female", 10),
+                new Employee("nelly", 18, "male", 10),
+                new Employee("nelly", 18, "male", 10));
+
+        List<Employee> employeeList2 = Arrays.asList(
+                new Employee("nelly", 18, "female", 10),
+                new Employee("nelly", 18, "male", 10),
+                new Employee("nelly", 18, "male", 10));
+
+        when(companyRepository.findAll()).thenReturn(asList(new Company("OOCL", employeeList),
+                new Company("SM", employeeList2)));
+
         CompanyService companyService = new CompanyService(companyRepository);
 
         //when
@@ -36,9 +54,14 @@ class CompanyServiceTest {
     @Test
     void should_return_company_with_name_OOCL_when_create_given_company_with_name_OOCL() {
         //given
-        Company newCompany = new Company(1, "OOCL");
-        CompanyRepositoryLegacy companyRepository = mock(CompanyRepositoryLegacy.class);
-        when(companyRepository.create(newCompany)).thenReturn(newCompany);
+        List<Employee> employeeList = Arrays.asList(
+                new Employee("nelly", 18, "female", 10),
+                new Employee("nelly", 18, "male", 10),
+                new Employee("nelly", 18, "male", 10));
+
+        Company newCompany = new Company("OOCL", employeeList);
+        ICompanyRepository companyRepository = mock(ICompanyRepository.class);
+        when(companyRepository.save(newCompany)).thenReturn(newCompany);
         CompanyService companyService = new CompanyService(companyRepository);
 
         //when
@@ -52,12 +75,15 @@ class CompanyServiceTest {
     @Test
     void should_return_company_with_name_OOCL_employees_2_employeesNumber_2_when_create_given_company_with_name_OOCL_employees_2() {
         //given
-        Company newCompany = new Company(1, "OOCL");
-        newCompany.addEmployee(new Employee());
-        newCompany.addEmployee(new Employee());
+        List<Employee> employeeList = Arrays.asList(
+                new Employee("nelly", 18, "female", 10),
+                new Employee("nelly", 18, "male", 10),
+                new Employee("nelly", 18, "male", 10));
 
-        CompanyRepositoryLegacy companyRepository = mock(CompanyRepositoryLegacy.class);
-        when(companyRepository.create(newCompany)).thenReturn(newCompany);
+        Company newCompany = new Company("OOCL", employeeList);
+
+        ICompanyRepository companyRepository = mock(ICompanyRepository.class);
+        when(companyRepository.save(newCompany)).thenReturn(newCompany);
         CompanyService companyService = new CompanyService(companyRepository);
 
         //when
@@ -66,40 +92,43 @@ class CompanyServiceTest {
         //then
         assertEquals("OOCL", company.getCompanyName());
         assertSame(newCompany, company);
-        assertEquals(2, company.getEmployeeNumber());
+        assertEquals(3, company.getEmployeeNumber());
     }
 
     @Test
     void should_return_company_when_searchByCompanyId_given_company_with_id_of_1() {
         //given
-        Company company = new Company(1, "OOCL");
-        CompanyRepositoryLegacy repository = mock(CompanyRepositoryLegacy.class);
-        when(repository.findById(company.getId())).thenReturn(company);
+        List<Employee> employeeList = Arrays.asList(
+                new Employee("nelly", 18, "female", 10),
+                new Employee("nelly", 18, "male", 10),
+                new Employee("nelly", 18, "male", 10));
+
+        Company company = new Company("OOCL", employeeList);
+        ICompanyRepository repository = mock(ICompanyRepository.class);
+        when(repository.findById(company.getId())).thenReturn(Optional.of(company));
         CompanyService companyService = new CompanyService(repository);
 
         //when
-        Company fetchedCompany = companyService.searchById(company.getId());
+        Optional<Company> fetchedCompany = companyService.searchById(company.getId());
 
         //then
         assertNotNull(company);
-        assertSame(company, fetchedCompany);
+        assertSame(company, fetchedCompany.orElse(null));
     }
 
     @Test
     void should_return_2_employee_under_OOCL_when_get_employees_given_2_employees() {
         //given
-        Company newCompany = new Company(1, "OOCL");
-        Employee firstEmployee = new Employee();
-        Employee secondEmployee = new Employee();
-        newCompany.addEmployee(firstEmployee);
-        newCompany.addEmployee(secondEmployee);
 
-        CompanyRepositoryLegacy companyRepository = mock(CompanyRepositoryLegacy.class);
-        when(companyRepository.getAll()).thenReturn(asList(newCompany));
+        Employee firstEmployee = new Employee("nelly", 18, "female", 10);
+        Employee secondEmployee = new Employee("nelly", 18, "female", 10);
+        Company newCompany = new Company("OOCL", Arrays.asList(firstEmployee, secondEmployee));
+        newCompany.setId(1);
+        ICompanyRepository companyRepository = mock(ICompanyRepository.class);
+        when(companyRepository.findById(1)).thenReturn(Optional.of(newCompany));
         CompanyService companyService = new CompanyService(companyRepository);
-
         //when
-        List<Employee> employees = companyService.getEmployeesByCompanyId(newCompany.getId());
+        List<Employee> employees = companyService.getEmployeesByCompanyId(1);
 
         //then
         assertEquals(2, employees.size());
@@ -110,18 +139,13 @@ class CompanyServiceTest {
     @Test
     void should_return_updated_company_with_retained_employee_when_update_given_company_with_two_employees_and_field_updates() {
         //given
-        Company company = new Company(1, "OOCL");
-        Employee firstEmployee = new Employee();
-        Employee secondEmployee = new Employee();
-        company.addEmployee(firstEmployee);
-        company.addEmployee(secondEmployee);
-
-        Company expectedCompany = new Company(1, "OOIL");
-        expectedCompany.addEmployee(firstEmployee);
-        expectedCompany.addEmployee(secondEmployee);
-
-        CompanyRepositoryLegacy companyRepository = mock(CompanyRepositoryLegacy.class);
-        when(companyRepository.update(company.getId(), expectedCompany)).thenReturn(expectedCompany);
+        Company company = new Company("OOCL", null);
+        company.setId(1);
+        Company expectedCompany = new Company("OOIL", null);
+        expectedCompany.setId(1);
+        ICompanyRepository companyRepository = mock(ICompanyRepository.class);
+        when(companyRepository.findById(1)).thenReturn(Optional.of(company));
+        when(companyRepository.save(company)).thenReturn(expectedCompany);
         CompanyService companyService = new CompanyService(companyRepository);
 
         //when
@@ -129,14 +153,14 @@ class CompanyServiceTest {
 
         //then
         assertSame(expectedCompany, updatedCompany);
-        assertEquals(2, updatedCompany.getEmployeeNumber());
     }
 
     @Test
     void should_trigger_repository_delete_once_when_service_delete_called_given_company_id() {
         //given
-        Company company = new Company(1, "OOCL");
-        CompanyRepositoryLegacy companyRepository = mock(CompanyRepositoryLegacy.class);
+        Company company = new Company("OOCL", null);
+        company.setId(1);
+        ICompanyRepository companyRepository = mock(ICompanyRepository.class);
 
         CompanyService companyService = new CompanyService(companyRepository);
 
@@ -144,17 +168,20 @@ class CompanyServiceTest {
         companyService.delete(company.getId());
 
         //then
-        verify(companyRepository, times(1)).delete(1);
+        verify(companyRepository, times(1)).deleteById(1);
     }
 
     @Test
     void should_return_2_companies_when_getCompaniesByPageAndPageSize_given_2_companies_page_1_and_pageSize_2() {
         //given
-        Company firstCompany = new Company(1, "OOCL");
-        Company secondCompany = new Company(1, "OOIL");
+        Company firstCompany = new Company("OOCL", null);
+        Company secondCompany = new Company("OOIL", null);
         int page = 1, pageSize = 2;
-        CompanyRepositoryLegacy companyRepository = mock(CompanyRepositoryLegacy.class);
-        when(companyRepository.getAll()).thenReturn(asList(firstCompany, secondCompany));
+        Pageable pageable = PageRequest.of(page - 1, pageSize);
+        ICompanyRepository companyRepository = mock(ICompanyRepository.class);
+        Page<Company> mockPage = mock(Page.class);
+        when(companyRepository.findAll(pageable)).thenReturn(mockPage);
+        when(companyRepository.findAll(pageable).toList()).thenReturn(asList(firstCompany, secondCompany));
         CompanyService companyService = new CompanyService(companyRepository);
 
         //when
