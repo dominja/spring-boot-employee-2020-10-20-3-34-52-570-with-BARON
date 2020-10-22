3d947feb32debd35dab529255cab14248f195844
Janelle <dominja@oocl.com>
2020-10-22 14:51:53 +0800

    [Company][Cedric-Janelle] feature: Added dependency

--- src/main/java/com/thoughtworks/springbootemployee/repository/IEmployeeRepository.java
+++ src/main/java/com/thoughtworks/springbootemployee/repository/IEmployeeRepository.java
@@ -1,9 +1,12 @@
 package com.thoughtworks.springbootemployee.repositories;
+
 import com.thoughtworks.springbootemployee.models.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public interface IEmployeeRepository extends JpaRepository <Employee,Integer>{
+import java.util.List;
 
+@Repository
+public interface IEmployeeRepository extends JpaRepository<Employee, Integer> {
+    List<Employee> findByGender(String gender);
 }
