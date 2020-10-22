5b577b19078ad5d0a585eb21f25bceb4f7cbb5ff
Cedric Don Coloma <colomce@oocl.com>
2020-10-22 15:48:59 +0800

    [JPA][Cedric-Janelle] feature: Implemented the company repository

--- src/main/java/com/thoughtworks/springbootemployee/services/CompanyService.java
+++ src/main/java/com/thoughtworks/springbootemployee/services/CompanyService.java
@@ -2,54 +2,59 @@ package com.thoughtworks.springbootemployee.services;
 
 import com.thoughtworks.springbootemployee.models.Company;
 import com.thoughtworks.springbootemployee.models.Employee;
-import com.thoughtworks.springbootemployee.repository.CompanyRepositoryLegacy;
+import com.thoughtworks.springbootemployee.repository.ICompanyRepository;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Optional;
 
 @Service
 public class CompanyService {
-    private CompanyRepositoryLegacy companyRepository;
+    private ICompanyRepository companyRepository;
 
-    public CompanyService(CompanyRepositoryLegacy companyRepository) {
+    public CompanyService(ICompanyRepository companyRepository) {
         this.companyRepository = companyRepository;
     }
 
     public List<Company> getAll() {
-        return companyRepository.getAll();
+        return companyRepository.findAll();
     }
 
     public Company create(Company newCompany) {
-        return companyRepository.create(newCompany);
+        return companyRepository.save(newCompany);
     }
 
-    public Company searchById(Integer id) {
+    public Optional<Company> searchById(Integer id) {
         return companyRepository.findById(id);
     }
 
     public List<Employee> getEmployeesByCompanyId(Integer id) {
-        return companyRepository.getAll()
-                .stream()
-                .filter(company -> company.getId().equals(id))
-                .findFirst()
-                .map(Company::getEmployees)
-                .orElse(Collections.emptyList());
+        Optional<Company> optionalCompany = companyRepository.findById(id);
+        if (optionalCompany.isPresent()) {
+            return optionalCompany.get().getEmployees();
+        }
+        return Collections.emptyList();
     }
 
     public Company update(Integer id, Company expectedCompany) {
-        return companyRepository.update(id, expectedCompany);
+        Optional<Company> optionalCompany = companyRepository.findById(id);
+        if (optionalCompany.isPresent()) {
+            optionalCompany.get().setCompanyName(expectedCompany.getCompanyName());
+            optionalCompany.get().setEmployees(expectedCompany.getEmployees());
+            return companyRepository.save(optionalCompany.get());
+        }
+        return null;
     }
 
     public void delete(Integer id) {
-        companyRepository.delete(id);
+        companyRepository.deleteById(id);
     }
 
     public List<Company> getCompaniesByPageAndPageSize(int page, int pageSize) {
-        return companyRepository.getAll().stream()
-                .skip(pageSize * (page - 1))
-                .limit(pageSize)
-                .collect(Collectors.toList());
+        Pageable pageable = PageRequest.of(page - 1, pageSize);
+        return companyRepository.findAll(pageable).toList();
     }
 }
