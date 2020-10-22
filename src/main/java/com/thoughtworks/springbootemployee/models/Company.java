fcc0cfc395205649891b5b933668483b5a718bdc
Cedric Don Coloma <colomce@oocl.com>
2020-10-22 15:47:55 +0800

    [JPA][Cedric-Janelle] feature: Added necessary annotations in Company Class

--- src/main/java/com/thoughtworks/springbootemployee/models/Company.java
+++ src/main/java/com/thoughtworks/springbootemployee/models/Company.java
@@ -1,17 +1,37 @@
 package com.thoughtworks.springbootemployee.models;
 
-import javax.persistence.*;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import java.util.List;
 
 @Entity
+@JsonPropertyOrder({"companyName", "employeeNumber", "employees"})
 public class Company {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @JsonIgnore
     private Integer id;
     private String companyName;
-    @OneToMany(fetch = FetchType.LAZY, orphanRemoval = true)
-    @JoinColumn(name="company_id")
+    @OneToMany(fetch = FetchType.LAZY, orphanRemoval = true, cascade = CascadeType.ALL)
+    @JoinColumn(name = "company_id")
     private List<Employee> employees;
+
+    public Company() {
+    }
+
+    public Company(String companyName, List<Employee> employees) {
+        this.companyName = companyName;
+        this.employees = employees;
+    }
 
     public String getCompanyName() {
         return companyName;
@@ -25,10 +45,6 @@ public class Company {
         return employees;
     }
 
-    public void addEmployee(Employee employee) {
-        employees.add(employee);
-    }
-
     public int getEmployeeNumber() {
         return employees.size();
     }
@@ -40,4 +56,9 @@ public class Company {
     public void setId(Integer id) {
         this.id = id;
     }
+
+    public void setEmployees(List<Employee> employees) {
+        this.employees = employees;
+    }
+
 }
