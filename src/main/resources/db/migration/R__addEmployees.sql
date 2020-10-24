55447cb852ae64c1a1eccff2ec7e4b6e701292f0
Janelle <dominja@oocl.com>
2020-10-24 11:20:43 +0800

    [JPA][Baron-Janelle] Scripts: Added Scripts needed to create table for company and employee and insert employees

---
+++ src/main/resources/db/migration/R__addEmployees.sql
@@ -0,0 +1,9 @@
+delete from employee;
+delete from company;
+INSERT INTO company(company_name) VALUES ('00CL');
+INSERT INTO employee(name, age, gender, salary, company_id) VALUES ('Janelle', 18, 'FeMale', 1000, 1);
+INSERT INTO employee(name, age, gender, salary, company_id) VALUES ('JC', 28, 'Female', 1000, 1);
+INSERT INTO employee(name, age, gender, salary, company_id) VALUES ('Cedric', 18, 'Male', 1000, 1);
+INSERT INTO employee(name, age, gender, salary, company_id) VALUES ('Joseph', 18, 'Female', 1000, 1);
+INSERT INTO employee(name, age, gender, salary, company_id) VALUES ('Charlie', 18, 'Male', 1000, 1);
+INSERT INTO employee(name, age, gender, salary, company_id) VALUES ('Woody', 18, 'Male', 1000, 1);
