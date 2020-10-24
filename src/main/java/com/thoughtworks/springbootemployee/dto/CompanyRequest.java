7b6f25d1dfe7e629316b033c5455826204e5d30f
BARON MANZANO <MANZABA@OOCL.COM>
2020-10-24 17:16:16 +0800

    [JPA][Baron-Janelle] Feature: added company dtos

--- src/main/java/com/thoughtworks/springbootemployee/dto/CompanyRequest.java
+++ src/main/java/com/thoughtworks/springbootemployee/dto/CompanyRequest.java
@@ -1,4 +1,36 @@
 package com.thoughtworks.springbootemployee.dto;
 
+import com.thoughtworks.springbootemployee.model.Employee;
+
+import java.util.List;
+
 public class CompanyRequest {
+    private String companyName;
+    private List<Employee> employees;
+
+    public CompanyRequest() {
+
+    }
+
+    public CompanyRequest(String companyName, List<Employee> employees) {
+        this.companyName = companyName;
+        this.employees = employees;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public List<Employee> getEmployees() {
+        return employees;
+    }
+
+    public int getEmployeeNumber() {
+        return employees.size();
+    }
+
 }
--- src/main/java/com/thoughtworks/springbootemployee/dto/CompanyResponse.java
+++ src/main/java/com/thoughtworks/springbootemployee/dto/CompanyResponse.java
@@ -1,6 +1,46 @@
 package com.thoughtworks.springbootemployee.dto;
 
+import com.thoughtworks.springbootemployee.model.Employee;
+
+import java.util.List;
+
 public class CompanyResponse {
-    public void setId(Integer id) {
+    private Integer id;
+    private String companyName;
+    private List<Employee> employees;
+
+    public CompanyResponse() {
+
     }
+
+    public CompanyResponse(String companyName, List<Employee> employees) {
+        this.companyName = companyName;
+        this.employees = employees;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public List<Employee> getEmployees() {
+        return employees;
+    }
+
+    public int getEmployeeNumber() {
+        return employees.size();
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+
 }
